136c912d66adc035712cc3d673baab676c4747b7
Rajnish <rajnish2804@gmail.com>
2022-10-22 18:07:42 +0530

    TAsk 1

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let [value,getvalue]= useState();
+  let [display,setvalue]=useState();
+  let inputvalue =(elem) =>{
+    getvalue(elem.target.value);
+  }
+  let fetchValue=() =>{
+        setvalue(display=value);
+  }
+ 
+  return (
+    <>
+    
+    <div className="Task1">
+       <h1> TASK 1</h1>     
+        <input type="text" value={value} onChange={inputvalue} />
+      <div>{display}</div>
+      <button onClick={fetchValue}>Submit</button>
+    </div>
+    </>
+  );
+}
+export default App;
